0f73823f2b3a063bf744ac04d9d3dd8be5e5b91e
CamBombarda <osoriocamila@live.com>
2024-11-15 20:12:30 +0000

    contadorok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function SecondsCounter({ hours, minutes, seconds }) {
+  return (
+    <div className="bigCounter">
+    <div>
+      <i class="far fa-clock"></i>
+      {String(hours).padStart(2, "0")}:
+      {String(minutes).padStart(2, "0")}:
+      {String(seconds).padStart(2, "0")}
+    </div>
+  </div>
+  );
+}
+
+function Home() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId); 
+  }, []);
+
+  const hours = Math.floor(seconds / 3600); 
+  const minutes = Math.floor((seconds % 3600) / 60); 
+  const displaySeconds = seconds % 60; // 
+
+  return <SecondsCounter hours={hours} minutes={minutes} seconds={displaySeconds} />;
+}
 
 export default Home;
